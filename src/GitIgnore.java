34d04378393b92425c8f9f6fc3dfdbb5a9482d40
Nurullah Karaköse <karakosenurullah@gmail.com>
2022-08-16 22:59:50 +0300

    V.M

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -2,5 +2,6 @@ public class GitIgnore {
     public static void main(String[] args) {
 
         System.out.println("GitIgnore deneme 1");
+        System.out.println("GitIgnore deneme 2");
     }
 }
